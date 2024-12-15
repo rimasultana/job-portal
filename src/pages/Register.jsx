fc7d0bfa2c5eef2bca7e69271af08044b07428dc
rimasultana <rimasultana20225@gmail.com>
2024-12-15 21:43:26 +0600

    some added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -30,6 +30,16 @@ const Register = () => {
                 className="input input-bordered"
                 required
               />
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">
                   Forgot password?
